f9bf65e6b87fcba408b452804d5035c89387b33a
Firman <fblazt@outlook.com>
2020-04-21 15:21:26 +0700

    Add cart item

---
+++ src/views/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <CartItem/>
+  </div>
+</template>
+
+<script>
+import CartItem from '@/components/CartItem.vue'
+
+export default {
+  name: 'Cart',
+  components: {
+    CartItem
+  }
+}
+</script>
+
+<style>
+
+</style>
