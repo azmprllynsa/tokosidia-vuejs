011d3d28f6a1c9dcd85fe0ba9ef2628183a7cb0b
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-04-22 07:01:45 +0700

    Add hover on category

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="tokosidia">
-    <nav id="nav">
-    </nav>
+    <NavbarHome />
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavbarHome from '@/components/NavbarHome.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    NavbarHome
+  }
+}
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/_reset.scss';
 
 .wrapper {
   width: 1200px;
-  margin: 100px auto 0;
+  margin: 105px auto 0;
 }
 </style>
