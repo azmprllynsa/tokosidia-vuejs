0eee6e90774e2a581608e67fb0d45e1cbb271702
Firman <fblazt@outlook.com>
2020-04-24 10:13:15 +0700

    Update user editing page and get user data

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
 
 export default new Vuex.Store({
   state: {
+    peopleDetail: []
   },
   mutations: {
+    SET_PEOPLE_DETAIL (state, peopleDetail) {
+      state.peopleDetail = peopleDetail
+    }
   },
   actions: {
+    loadPeopleDetail ({ commit }) {
+      axios
+        .get('http://192.168.1.84:8000/api/v1/tokosidia/user/1')
+        .then(res => {
+          console.log(res.data)
+          const peopleDetail = res.data
+          commit('SET_PEOPLE_DETAIL', peopleDetail)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
